fdef1ba4d65bfad2dcf25c13897ae8c35c8f20d9
Nupoor Santosh Shetye <shetyenupoor10@gmail.com>
2023-04-13 02:21:52 +0530

    Created a color generator app

---
+++ README.md
@@ -0,0 +1,9 @@
+# COLOR GENERATOR
+
+Created a simple random color generator that random generates a color based on RGB values
+
+## 💻 Built With
+
+➡ HTML
+➡ CSS
+➡ JavaScript
---
+++ script.js
@@ -0,0 +1,8 @@
+function myfunc() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    document.body.style.backgroundColor = color;
+    document.querySelector("#color-text").innerHTML = `${color}`;
+}
